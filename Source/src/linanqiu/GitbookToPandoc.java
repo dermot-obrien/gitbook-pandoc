60a72d488f0c458d44dc8ee514f211fd6d3c9f49
Sylvain Hallé <shalle@acm.org>
2017-07-24 08:00:51 -0400

    Restructured folder and added build script

--- Source/src/linanqiu/GitbookToPandoc.java
+++ Source/src/linanqiu/GitbookToPandoc.java
@@ -1,3 +1,21 @@
+/*
+  gitbook-pandoc
+  Copyright (C) 2014-2017 linanqiu
+  Copyright (C) 2017 Sylvain Hallé
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,6 +32,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
+
+package linanqiu;
 
 /**
  * Takes a Gitbook directory and a output directory, and converts all markdowns
